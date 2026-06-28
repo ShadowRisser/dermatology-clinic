227fa4bbf3489510462324f9224e49a2d2c81795
Saim Mirza <saim@shadowriser.com>
2026-06-28 10:31:44 +0500

    feat: deploy static site

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,15 @@
-     1|import type { NextConfig } from "next";
-     2|
-     3|const nextConfig: NextConfig = {
-     4|  output: "export",
-     5|  /* config options here */
-     6|  typescript: {
-     7|    ignoreBuildErrors: true,
-     8|  },
-     9|  reactStrictMode: false,
-    10|  basePath: "/dermatology-clinic",
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+  basePath: "/dermatology-clinic",
   assetPrefix: "/dermatology-clinic/",
   images: { unoptimized: true },
 };
-    11|
-    12|export default nextConfig;
-    13|
+
+export default nextConfig;
